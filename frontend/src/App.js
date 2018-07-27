cd110d2d565afe02bcccc7a38602e6d28cd97aff
arac-codespace <alex.aguirre0026@gmail.com>
2018-07-27 18:49:53 -0400

    Setup react router.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {Route, Switch, BrowserRouter, Link} from 'react-router-dom';
+import {withRouter} from 'react-router';
+
+
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home/>
+	      <div className="container-fluid">
+	        <div className="row">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route component={NotFound} />
+            </Switch>
+	        </div>
+	      </div>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
